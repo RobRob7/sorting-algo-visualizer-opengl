d742fe36027c353636530a4b48bf7a05aeb8bb75
RobRob7 <rjames11@nmsu.edu>
2025-09-15 19:06:38 -0600

    updated initial window width + height

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "controller/appcontroller/appcontroller.h"
 
 // window settings
-unsigned int width = 800;
-unsigned int height = 600;
+unsigned int width = 1920;
+unsigned int height = 1080;
 const char* windowName = "Sorting Visualizer OpenGL";
 
 // main driver
